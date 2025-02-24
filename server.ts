df9d675d5a273460ee83d89e06d08bc964d26a70
Henry Blanchette <henry@monkeyface.com>
2025-02-24 13:53:00 -0500

    fixed urls

--- server.ts
+++ server.ts
@@ -5,7 +5,8 @@ const server = Bun.serve({
   async fetch(req) {
     const url_str = req.url.endsWith("/") ? `${req.url}index.html` : req.url
     const url = new URL(url_str);
-    const filePath = `docs${url.pathname}`;
+    var filePath = `docs${url.pathname}`;
+    filePath = filePath.replace("/llm-toys", "")
     console.log(`GET ${filePath}`)
     const file = Bun.file(filePath);
     if (!(await file.exists())) return new Response(`Not Found: ${url_str}`, { status: 404 });
