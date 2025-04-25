48bb9ffec6d4d673d5275f451b2e49199c864966
Henry Blanchette <henry@monkeyface.com>
2025-04-25 12:46:45 -0400

    checkpoint

--- experiments/openai_and_zod.ts
+++ experiments/openai_and_zod.ts
@@ -5,13 +5,33 @@ function check<ZodInput extends ZodType>(schema: ZodInput) {
   console.log(JSON.stringify(zodResponseFormat(schema, "schema"), undefined, 4))
 }
 
+const Person_schema = z.object({
+  name: z.string(),
+  age: z.number().int(),
+})
+type Person = z.infer<typeof Person_schema>
+
+const example_person: Person = {
+  "name": "Bobert",
+  "age": 1,
+}
+
+const example_untype_person: any = {
+  "name": "Bobert",
+  "age": 1
+}
+
+// const example_typed_person = Person_schema.parse(example_untype_person)
+
+// check(Person_schema)
+
 // check(z.object({ name: z.string(), age: z.bigint() }))
 // check(z.enum(["a", "b", "c"]))
 // check(z.union([z.number(), z.string()]))
-// check(z.union([z.object({ x: z.number() }), z.object({ y: z.string() })]))
+check(z.union([z.object({ x: z.number() }), z.object({ y: z.string() })]))
 // check(z.intersection(z.object({ x: z.number() }), z.object({ y: z.string() })))
 // check(z.literal("hello"))
 // check(z.tuple([z.string(), z.string(), z.string()]))
 // check(z.boolean())
 // check(z.object({ arg: z.array(z.string()) }))
-check(z.object({ arg: z.nullable(z.string()) }))
+// check(z.object({ arg: z.nullable(z.string()) }))
---
+++ notes/notes_tmp.md
@@ -0,0 +1,3 @@
+## Mutable World
+
+## Manager Sim
